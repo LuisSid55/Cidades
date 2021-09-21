efd690bc5bc61f631f8717723032b27852433361
LuisSid55 <luiscitty4@gmail.com>
2021-09-21 17:03:14 -0300

    Adição das cidades do litoral sul de SP

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Cidades do litoral sul de São Paulo");
+            Console.WriteLine();
+            Console.WriteLine("Santos");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("Cubatão");
+            Console.WriteLine("Caraguatatuba");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Ubatuba");
+            Console.WriteLine("São Sebastião");
+            Console.WriteLine("Peruibe");
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Ilhabela");
+            Console.WriteLine("Iguape");
+            Console.WriteLine("Cananéia");
+            Console.WriteLine("Ilha Comprida");
+        }
+    }
+}
